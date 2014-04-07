49ff83c6f00aa628728e1fbd7ccce3d64b4bbabe
Celeste Bertin <celbertin@gmail.com>
2014-04-07 00:46:38 -0300

    server funcional

--- src/com/redes/tarea1/Servidor.java
+++ src/com/redes/tarea1/Servidor.java
@@ -1,78 +1,235 @@
 package com.redes.tarea1;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Servidor
 {
-	int puerto = 8080;
+	int puerto = 90;
+	
+	final int ERROR = 0;
+	final int WARNING = 1;
+	final int DEBUG = 2;
+	
+		
+	// funcion para centralizar los mensajes de depuracion
+
+	void depura(String mensaje)
+	{
+		depura(mensaje,DEBUG);
+	}	
+
+	void depura(String mensaje, int gravedad)
+	{
+		System.out.println("Mensaje: " + mensaje);
+	}	
+		
+	// punto de entrada a nuestro programa
+	public static void main(String [] array)	
+	{
+		Servidor instancia = new Servidor(array);	
+		instancia.arranca();
+	}
+	
+	// constructor que interpreta los parameros pasados
+	Servidor(String[] param)
+	{
+		procesaParametros();	
+	}
+	
+	// parsearemos el fichero de entrada y estableceremos las variables de clase
+	boolean procesaParametros()
+	{
+		return true;	
+	}
+	
+	boolean arranca()
+	{
+		depura("Arrancamos nuestro servidor",DEBUG);
+		
+		try
+		{
+		
+			
+			ServerSocket s = new ServerSocket(90);
+
+			depura("Quedamos a la espera de conexion");
+			
+			while(true)  // bucle infinito .... ya veremos como hacerlo de otro modo
+			{
+				Socket entrante = s.accept();
+				peticionWeb pCliente = new peticionWeb(entrante);
+				pCliente.start();
+			}
+			
+		}
+		catch(Exception e)
+		{
+			depura("Error en servidor\n" + e.toString());
+		}
+		
+		return true;
+	}
+	
+}
+
+
+
+class peticionWeb extends Thread
+{
+	int contador = 0;
 
 	final int ERROR = 0;
 	final int WARNING = 1;
 	final int DEBUG = 2;
 
-        void depura(String mensaje) // los mensajes por defecto serán en modo depuracion
+	void depura(String mensaje)
 	{
 		depura(mensaje,DEBUG);
-	} 
+	}	
 
-	// funcion para centralizar los mensajes de depuración
-        	void depura(String mensaje, int gravedad)
-        	{
-        System.out.println("Mensaje: " + mensaje);
-	} 
-
-	// punto de entrada a nuestro programa
-        public static void main(String [] array) 
+	void depura(String mensaje, int gravedad)
 	{
-		Servidor instancia = new Servidor(array); 
-		instancia.arranca();
-	}
+		System.out.println(currentThread().toString() + " - " + mensaje);
+	}	
 
-	// constructor que interpreta los parameros pasados
-        Servidor(String[] param)
+	private Socket scliente 	= null;		// representa la petición de nuestro cliente
+   	private PrintWriter out 	= null;		// representa el buffer donde escribimos la respuesta
+
+   	peticionWeb(Socket ps)
+   	{
+		depura("El contador es " + contador);
+		
+		contador ++;
+		
+		
+		
+		scliente = ps;
+		setPriority(NORM_PRIORITY - 1); // hacemos que la prioridad sea baja
+   	}
+
+	public void run() // emplementamos el metodo run
 	{
-		procesaParametros(); 
-	}
+		depura("Procesamos conexion");
 
-	// parsearemos el fichero de entrada y estableceremos las variables de clase
-        boolean procesaParametros()
+		try
+		{
+			BufferedReader in = new BufferedReader (new InputStreamReader(scliente.getInputStream()));
+  			out = new PrintWriter(new OutputStreamWriter(scliente.getOutputStream(),"8859_1"),true) ;
+
+
+			String cadena = "";		// cadena donde almacenamos las lineas que leemos
+			int i=0;				// lo usaremos para que cierto codigo solo se ejecute una vez
+	
+			do			
+			{
+				cadena = in.readLine();
+
+				if (cadena != null )
+				{
+					// sleep(500);
+					depura("--" + cadena + "-");
+				}
+
+
+				if(i == 0) // la primera linea nos dice que fichero hay que descargar
+				{
+			        i++;
+			        
+			        StringTokenizer st = new StringTokenizer(cadena);
+                    
+                    if ((st.countTokens() >= 2) && st.nextToken().equals("GET")) 
+                    {
+                    	retornaFichero(st.nextToken()) ;
+                    }
+                    else 
+                    {
+                    	out.println("400 Petición Incorrecta") ;
+                    }
+				}
+				
+			}
+			while (cadena != null && cadena.length() != 0);
+
+		}
+		catch(Exception e)
+		{
+			depura("Error en servidor\n" + e.toString());
+		}
+			
+		depura("Hemos terminado");
+	}
+	
+	
+	void retornaFichero(String sfichero)
 	{
-		return true; 
-	}
-
-        boolean arranca()
+		depura("Recuperamos el fichero " + sfichero);
+		
+		// comprobamos si tiene una barra al principio
+		if (sfichero.startsWith("/"))
+		{
+			sfichero = sfichero.substring(1) ;
+		}
+        
+        // si acaba en /, le retornamos el index.htm de ese directorio
+        // si la cadena esta vacia, no retorna el index.htm principal
+        if (sfichero.endsWith("/") || sfichero.equals(""))
         {
-        	depura("Arrancamos nuestro servidor",DEBUG);
-        	try
-        	{
-        		ServerSocket s = new ServerSocket(90);
-        		depura("Quedamos a la espera de conexion");
-        		Socket entrante = s.accept();
-        		depura("Procesamos conexion");
-        		BufferedReader in = new BufferedReader (new InputStreamReader(entrante.getInputStream()));
-        		String cadena = "";
-
-        		while (cadena != null)
-        		{
-        			cadena = in.readLine();
-        			if (cadena != null)
-        			{
-        				depura("--" + cadena);
-        			}
-        		}
-
-        		depura("Hemos terminado");
-
-        	}
-        	catch(Exception e)
-        	{
-        		depura("Error en servidor\n" + e.toString());
-        	}
-        		return true;
-        	}
+        	sfichero = sfichero + "index.htm" ;
         }
-        	
+        
+        try
+        {
+	        
+		    // Ahora leemos el fichero y lo retornamos
+		    File mifichero = new File(sfichero) ;
+		        
+		    if (mifichero.exists()) 
+		    {
+	      		out.println("HTTP/1.0 200 ok");
+				out.println("Server: Roberto Server/1.0");
+				out.println("Date: " + new Date());
+				out.println("Content-Type: text/html");
+				out.println("Content-Length: " + mifichero.length());
+				out.println("\n");
+			
+				BufferedReader ficheroLocal = new BufferedReader(new FileReader(mifichero));
+				
+				
+				String linea = "";
+				
+				do			
+				{
+					linea = ficheroLocal.readLine();
+	
+					if (linea != null )
+					{
+						// sleep(500);
+						out.println(linea);
+					}
+				}
+				while (linea != null);
+				
+				depura("fin envio fichero");
+				
+				ficheroLocal.close();
+				out.close();
+				
+			}  // fin de si el fiechero existe 
+			else
+			{
+				depura("No encuentro el fichero " + mifichero.toString());	
+	      		out.println("HTTP/1.0 400 ok");
+	      		out.close();
+			}
+			
+		}
+		catch(Exception e)
+		{
+			depura("Error al retornar fichero");	
+		}
 
+	}
+	
+}
